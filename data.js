66c586aef1d5bfd126f64640dc5f106710c4be2a
alekcandr1 <alekcandrmain@gmail.com>
2023-12-14 17:29:33 +0300

    add filter

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const songs = [
+let songs = [
     { year: 2005, artist: 'Coldplay', title: 'Fix You' },
     { year: 1983, artist: 'Michael Jackson', title: 'Billie Jean' },
     { year: 1997, artist: 'Radiohead', title: 'Paranoid Android' },
@@ -13,7 +13,13 @@ export function subscribe(subscriberCallback) {
 }
 
 
-
+// getter
 export function getSongs() {
     return songs;
+}
+
+// setter
+export function setFilter(searchTerm) {
+    songs = songs.filter(s => s.title.indexOf(searchTerm) > -1 || s.artist.indexOf(searchTerm) > -1);
+    subscriber();
 }
--- src/components/filter-panel.js
+++ src/components/filter-panel.js
@@ -1,5 +1,20 @@
+import { setFilter } from '../../data.js';
+
 export function renderFilterPanel () {
     const filterPanelElement = document.createElement('div');
-    filterPanelElement.innerHTML = 'Panel';
+
+    const searchInput = document.createElement('input');
+    searchInput.placeholder = 'Artist or song title';
+
+    const searchButton = document.createElement('button');
+    searchButton.innerText = 'Search';
+
+    searchButton.addEventListener('click', () => {
+        setFilter(searchInput.value);
+    });
+
+    filterPanelElement.appendChild(searchInput);
+    filterPanelElement.appendChild(searchButton);
+
     return filterPanelElement;
 }
